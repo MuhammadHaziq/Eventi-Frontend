59cdde784b0a396db8cade57d9d725863ab900ed
MuhammadHaziq <muhammadhaziq341@gmail.com>
2023-05-13 21:08:39 +0500

    remove commands

--- src/components/AppBreadcrumb.js
+++ src/components/AppBreadcrumb.js
@@ -33,7 +33,6 @@ const AppBreadcrumb = () => {
 
   return (
     <CBreadcrumb className="m-0 ms-2">
-      <CBreadcrumbItem href="/">Registration</CBreadcrumbItem>
       {breadcrumbs.map((breadcrumb, index) => {
         return (
           <CBreadcrumbItem
