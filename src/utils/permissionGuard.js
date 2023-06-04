b163333ebb9cdf2d899b56056295401b397a22a6
MuhammadHaziq <muhammadhaziq341@gmail.com>
2023-06-04 14:47:29 +0500

    remove console

--- src/utils/permissionGuard.js
+++ src/utils/permissionGuard.js
@@ -8,12 +8,6 @@ const permissionGuard = (permissionCode) => {
         (currentUser.permissions || []).findIndex(
           (item) => item !== null && item.permission === permissionCode
         );
-      console.log(
-        userPermission,
-        currentUser,
-        permissionCode,
-        userPermission !== -1
-      );
       if (userPermission !== -1) {
         return true;
       } else {
