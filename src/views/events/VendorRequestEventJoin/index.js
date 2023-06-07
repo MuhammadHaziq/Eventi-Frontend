17ff99c5ee3922adef14fc5c30722f7eb26392a4
MuhammadHaziq <muhammadhaziq341@gmail.com>
2023-06-08 00:40:46 +0500

    update joined vendor view

--- src/views/events/VendorRequestEventJoin/index.js
+++ src/views/events/VendorRequestEventJoin/index.js
@@ -6,7 +6,7 @@ import AppEventUserDetail from "src/components/AppEventUserDetail";
 import ProductDetail from "./ProductDetail";
 import { useParams } from "react-router-dom";
 import { AppToast } from "src/components/AppToast";
-import { getJoinedVendor } from "src/context/EventContext/service";
+import { getEvent, getJoinedVendor } from "src/context/EventContext/service";
 import AppEventDetail from "src/components/AppEventDetail";
 const VendorRequestEventJoin = () => {
   const { currentUser } = useAppState();
@@ -76,11 +76,60 @@ const VendorRequestEventJoin = () => {
     }
   }, [account_id]);
 
+  const getEventDetail = useCallback(() => {
+    try {
+      setIsLoading(true);
+      getEvent(event_id)
+        .then((response) => {
+          if (response.data.data) {
+            setEventDetail(response.data.data || null);
+            app_dispatch({
+              type: "SHOW_RESPONSE",
+              toast: AppToast({
+                message: response.data.message,
+                color: "success-alert",
+              }),
+            });
+          } else {
+            setEventDetail(null);
+            app_dispatch({
+              type: "SHOW_RESPONSE",
+              toast: AppToast({
+                message: response.data.message,
+                color: "danger-alert",
+              }),
+            });
+          }
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setEventDetail(null);
+          setIsLoading(false);
+          app_dispatch({
+            type: "SHOW_RESPONSE",
+            toast: AppToast({ message: err.message, color: "danger-alert" }),
+          });
+        });
+    } catch (err) {
+      setIsLoading(false);
+      app_dispatch({
+        type: "SHOW_RESPONSE",
+        toast: AppToast({ message: err.message, color: "danger-alert" }),
+      });
+    }
+  }, [event_id]);
+
   useEffect(() => {
     if (account_id) {
       getJoinedEventDetail(account_id);
     }
   }, [account_id]);
+
+  useEffect(() => {
+    if (event_id) {
+      getEventDetail(event_id);
+    }
+  }, [event_id]);
 
   return (
     <>
@@ -101,7 +150,7 @@ const VendorRequestEventJoin = () => {
                   }
                 />
               </CCol> */}
-              <AppEventDetail event_detail={eventDetail} />
+              {eventDetail && <AppEventDetail event_detail={eventDetail} />}
               <ProductDetail
                 joined_event_id={joined_event_id}
                 eventProducts={selectedProducts}
